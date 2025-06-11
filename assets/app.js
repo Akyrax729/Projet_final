fa95f99e997df7bcefc9ec4a1ccd2f14f2b0d607
Ben <ben24france@gmail.com>
2025-06-12 00:06:24 +0200

    namechange et ajout contact

--- assets/app.js
+++ assets/app.js
@@ -16,66 +16,66 @@ console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 /**
 * Vérifie si le thème est stocké dans le localStorage ou s'il faut utiliser les paramètres par défaut
 */
-function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
-  if (localStorageTheme !== null) {
-    return localStorageTheme;
+  function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+    if (localStorageTheme !== null) {
+      return localStorageTheme;
+    }
+
+    if (systemSettingDark.matches) {
+      return "dark";
+    }
+
+    return "light";
   }
 
-  if (systemSettingDark.matches) {
-    return "dark";
+  /**
+  * Change le texte de l'input
+  */
+  function updateButton({ buttonLD, isDark }) {
+    const newCta = isDark ? "Light theme" : "Dark theme";
+    buttonLD.setAttribute("aria-label", newCta);
+    buttonLD.innerText = newCta;
   }
 
-  return "light";
-}
-
-/**
-* Change le texte de l'input
-*/
-function updateButton({ buttonLD, isDark }) {
-  const newCta = isDark ? "Light theme" : "Dark theme";
-  buttonLD.setAttribute("aria-label", newCta);
-  buttonLD.innerText = newCta;
-}
-
-/**
-* Donne la classe Data-theme à l'élément (balise) HTML
-*/
-function updateThemeOnHtmlEl({ theme }) {
-  document.querySelector("html").setAttribute("data-theme", theme);
-}
+  /**
+  * Donne la classe Data-theme à l'élément (balise) HTML
+  */
+  function updateThemeOnHtmlEl({ theme }) {
+    document.querySelector("html").setAttribute("data-theme", theme);
+  }
 
 
-/**
-* On page load:
-*/
+  /**
+  * On page load:
+  */
 
-/**
-* 1. Récupère la valeur du thème dans le localStorage ou les paramètres système
-*/
-const button = document.querySelector("[data-theme-toggle]");
-const localStorageTheme = localStorage.getItem("theme");
-const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+  /**
+  * 1. Récupère la valeur du thème dans le localStorage ou les paramètres système
+  */
+  const button = document.querySelector("[data-theme-toggle]");
+  const localStorageTheme = localStorage.getItem("theme");
+  const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-/**
-* 2. Met le site dans le thème correspondant
-*/
-let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+  /**
+  * 2. Met le site dans le thème correspondant
+  */
+  let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
 
-/**
-* 3. Met à jour le texte du bouton et l'attribut data-theme de l'élément HTML si le thème est "sombre"
-*/
-updateButton({ buttonLD: button, isDark: currentThemeSetting === "dark" });
-updateThemeOnHtmlEl({ theme: currentThemeSetting });
+  /**
+  * 3. Met à jour le texte du bouton et l'attribut data-theme de l'élément HTML si le thème est "sombre"
+  */
+  updateButton({ buttonLD: button, isDark: currentThemeSetting === "dark" });
+  updateThemeOnHtmlEl({ theme: currentThemeSetting });
 
-/**
-* 4. Ajout de l'événement pour changer le thème au clic sur le bouton
-*/
-button.addEventListener("click", (event) => {
-  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+  /**
+  * 4. Ajout de l'événement pour changer le thème au clic sur le bouton
+  */
+  button.addEventListener("click", (event) => {
+    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
 
-  localStorage.setItem("theme", newTheme);
-  updateButton({ buttonLD: button, isDark: newTheme === "dark" });
-  updateThemeOnHtmlEl({ theme: newTheme });
+    localStorage.setItem("theme", newTheme);
+    updateButton({ buttonLD: button, isDark: newTheme === "dark" });
+    updateThemeOnHtmlEl({ theme: newTheme });
 
-  currentThemeSetting = newTheme;
-}); 
+    currentThemeSetting = newTheme;
+  }); 
